07b82dd0bf03b389a03ef1386fa2240976acf250
nks <nsin08@yahoo.com>
2017-07-25 13:20:37 +0530

    sample

---
+++ commic/Factorial.swift
@@ -0,0 +1,18 @@
+//
+//  Factorial.swift
+//  commic
+//
+//  Created by nks on 25/07/17.
+//  Copyright © 2017 nks. All rights reserved.
+//
+
+import Foundation
+
+
+open class Factorial {
+    
+    func fact(num: Int ) -> Int  {
+        
+        return num * 2
+    }
+}
